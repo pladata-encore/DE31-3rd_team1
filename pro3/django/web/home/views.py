d9675e5bba03e7771b10c97bd15dc2c33567fe19
soyoungshin <ccvv0215@gmail.com>
2024-07-02 12:14:53 +0000

    merged with sy

--- pro3/django/web/home/views.py
+++ pro3/django/web/home/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import os
 
 
 def index(request):
     #return HttpResponse("첫 화면이 될 페이지")
     return render(request, 'home/index.html')
+
+def get_wordcloud_image(request):
+    date = request.GET.get('input-date')
+    if date:
+        image_path = os.path.join('static/images', f'{date}.png')
+        if os.path.exists(image_path):
+            image_url = f'/static/images/{date}.png'
+        else:
+            image_url = '/static/images/basic.png'
+    else:
+        image_url = '/static/images/basic.png'
+
+    return JsonResponse({'image_url': image_url})
