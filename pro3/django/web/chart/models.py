5fd87fb46e207c02c52b7abd81fbbadf7553e644
alsgur <alsgur1101@gmail.com>
2024-07-02 17:47:18 +0900

    second

--- pro3/django/web/chart/models.py
+++ pro3/django/web/chart/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
-class SalesData(models.Model):
-    date = models.DateField()
-    sales = models.DecimalField(max_digits=10, decimal_places=2)
 
-    def __str__(self):
-        return f"{self.date}: {self.sales}"
+# class SalesData(models.Model):
+#     date = models.DateField()
+#     sales = models.DecimalField(max_digits=10, decimal_places=2)
+
+#     def __str__(self):
+#         return f"{self.date}: {self.sales}"
+    
+
+class Test(models.Model):
+    keywords = models.TextField(db_column='Keywords', blank=True, primary_key=True)  # Field name made lowercase.
+    count = models.BigIntegerField(db_column='Count', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'test'
--- pro3/django/web/chart/urls.py
+++ pro3/django/web/chart/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import chart_view
 from . import views
 
 urlpatterns = [
-    path('', views.chart_view, name='chart_view'),  # /chart/ 경로에 대응하는 뷰 설정
+    path('', views.chart_view, name='chart'),  # /chart/ 경로에 대응하는 뷰 설정
 ]
+
+
