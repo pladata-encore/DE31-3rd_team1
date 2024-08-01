67cc8f4a12da504c29081bc67f6118f5780cb9ce
soyoungshin <ccvv0215@gmail.com>
2024-08-01 16:42:29 +0000

    hide db

---
+++ django/web/config/my_setting.py
@@ -0,0 +1,14 @@
+#my_settings.py
+ 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DBNAME',
+        'USER': 'class5',
+        'PASSWORD': '123',
+        'HOST': '43.202.5.70',
+        'PORT': '3306',
+        
+    }
+}
+SECRET_KEY = "django-insecure-jxlcq=s7(1%yt&81%9&w+xjmsxt@941s!zgs0(wa^g#e+-8y^("
